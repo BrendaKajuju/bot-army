97fabeecf4c8e1a4b2877a44069d4c9b4b34f0da
Brenda <brendakajuju777@gmail.com>
2023-05-03 07:22:51 +0300

    add json server

--- src/components/BotCard.js
+++ src/components/BotCard.js
@@ -1,16 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Card, Grid, Row, Text, Button, Spacer, Progress } from "@nextui-org/react";
 
 const BotCard = ({robot}) => {
-    const handleAddBot = (event) => {
-        event.preventDefault();
 
-        const newRobot = {
-
-        }
-    }
-  return (
-    <Grid.Container gap={2} justify="flex-start">
+return (
+    <Grid>
+<Grid.Container gap={2} justify="flex-start" >
         <Grid xs={6} sm={3}>
           <Card isPressable>
             <Card.Body css={{ mt: 0 }}>
@@ -18,13 +13,12 @@ const BotCard = ({robot}) => {
                 src={robot.avatar_url}
                 objectFit="cover"
                 width="100%"
-                height={200}
+                height={100}
                 alt={robot.bot_class}
               />
             <Text b>{robot.name}</Text>
-            <Text css={{ color: "$accents7", fontWeight: "$semibold", fontSize: "$sm" }}>
-                  {robot.bot_class}
-                </Text>
+            <Text css={{ color: "$accents7", fontWeight: "$semibold", fontSize: "$sm" }}>{robot.bot_class}</Text>
+            <Text css={{ color: "$accents7", fontWeight: "$semibold", fontSize: "$sm" }}>{robot.catchphrase}</Text>
                 <Grid>
                 <Text color="success" small b>Health</Text>
                 <Progress value={robot.health} color="success" status="success" />
@@ -36,16 +30,19 @@ const BotCard = ({robot}) => {
             </Card.Body>
             <Card.Footer css={{ justifyItems: "flex-start" }}>
               <Row wrap="wrap" justify="space-between" align="center">
-
               </Row>
               <Spacer y={2} />
               <Row wrap="wrap" justify="space-between" align="center">
               <Button color="gradient" auto ghost>Join the Force</Button>
+              <Spacer y={1} />
+              <Button color="gradient" auto ghost>Remove robot</Button>
               </Row>
             </Card.Footer>
           </Card>
         </Grid>
-    </Grid.Container>)
+      </Grid.Container>
+    </Grid>
+    )
 }
 
 export default BotCard
--- src/components/BotCollection.js
+++ src/components/BotCollection.js
@@ -1,46 +1,50 @@
-import React, { useState, Fragment } from 'react';
+import React, { useState, useEffect } from 'react';
 import BotCard from './BotCard';
 import robot from "../database/db.json"
+import YourBotArmy from './yourbotarmy';
+import { nanoid } from 'nanoid';
 
 function BotCollection () {
 
 // Retrieve data from the db.json file when adding and editing files
 const [robots, setRobots] = useState (robot);
-const [addBots, setBots] = useState (robot);
-const [editBotId, setBotId] = useState(null);
-const [editBotData, setBotData] = useState (
-    {
-        name: '',
-        health: '',
-        damage: '',
-        armor: '',
-        bot_class: '',
-        catchphrase: '',
-        avatar_url: '',
-        created_at: '',
-        updated_at: ''
-    }
+const [editRobotId, setRobotId] = useState(null);
+const [addData, setBotData] = useState (
+  {
+      name: '',
+      health: '',
+      damage: '',
+      armor: '',
+      bot_class: '',
+      catchphrase: '',
+      avatar_url: ''
+  }
 );
 
 // Create a function to add bot to the collection
 const handleAddBot = (event, robot) => {
-    event.preventDefault();
-    setBots(robot.id);
+event.preventDefault();
+setRobotId(robot.id);
 
-    const robotarmy = {
-
-    }
+const robotarmy = {
+  key: robot.id,
+  name: robot.name,
+  health: robot.health,
+  damage: robot.damage,
+  armor: robot.armor,
+  bot_class: robot.bot_class,
+  catchphrase: robot.catchphrase,
+  avatar_url: robot.avatar_url
+}
+setBotData(robotarmy)
 }
   return (
     <div className='bot-container'>
         {robots.map((robot) => (
-            <Fragment>
-            <BotCard 
-            robot = {robot} />
+          <Fragment>
+           <BotCard  robot = {robot}/>
           </Fragment>
-        ))
-        }
+        ))}
     </div>
-  )
-}
+)}
 export default BotCollection

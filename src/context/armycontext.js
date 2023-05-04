f71d9c660424ae9bf3c1e6168e69c704b368b971
Brenda <brendakajuju777@gmail.com>
2023-05-04 15:55:32 +0300

    add context

---
+++ src/context/armycontext.js
@@ -0,0 +1,10 @@
+//Provide context that will handle the addition of bots into the army
+import { createContext, useReducer } from "react";
+
+export const context = createContext();
+
+//Add a function that will automatically add robots to the army
+
+export const Context = (props) => {
+    
+}

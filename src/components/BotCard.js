ef41716efe2c01080be645a92d27991ca788e873
Breeze <126792521+BrendaKajuju@users.noreply.github.com>
2023-05-04 23:29:57 +0000

    Update 4 files

--- src/components/BotCard.js
+++ src/components/BotCard.js
@@ -1,118 +1,16 @@
-//component used to display all bots
-import React, { useState, useEffect, useContext } from 'react';
-import axios from "axios";
-import { Botcontext } from "../context/Context"
-import { Card, Grid, Row, Text, Button, Spacer, Progress } from "@nextui-org/react";
+import { Route, Routes } from "react-router";
+import YourBotArmy from "../components/yourbotarmy";
+import BotCard from "../components/BotCard";
 
-<<<<<<< HEAD
-const BotCard = () => {
-  //Retrieve data from db.json
-  const [data, setdata] = useState([]);
-=======
-const BotCard = ({robot}) => {
-
-return (
-    <Grid>
-<Grid.Container gap={2} justify="flex-start" >
-        <Grid xs={6} sm={3}>
-          <Card isPressable>
-            <Card.Body css={{ mt: 0 }}>
-              <Card.Image
-                src={robot.avatar_url}
-                objectFit="cover"
-                width="100%"
-                height={100}
-                alt={robot.bot_class}
-              />
-            <Text b>{robot.name}</Text>
-            <Text css={{ color: "$accents7", fontWeight: "$semibold", fontSize: "$sm" }}>{robot.bot_class}</Text>
-            <Text css={{ color: "$accents7", fontWeight: "$semibold", fontSize: "$sm" }}>{robot.catchphrase}</Text>
-                <Grid>
-                <Text color="success" small b>Health</Text>
-                <Progress value={robot.health} color="success" status="success" />
-                <Text color="error" small b>Damage</Text>
-                <Progress value={robot.damage} color="error" status="error" />
-                <Text color="warning" small b>Armor</Text>
-                <Progress value={robot.damage} color="warning" status="warning" />
-               </Grid>
-            </Card.Body>
-            <Card.Footer css={{ justifyItems: "flex-start" }}>
-              <Row wrap="wrap" justify="space-between" align="center">
-              </Row>
-              <Spacer y={2} />
-              <Row wrap="wrap" justify="space-between" align="center">
-              <Button color="gradient" auto ghost>Join the Force</Button>
-              <Spacer y={1} />
-              <Button color="gradient" auto ghost>Remove robot</Button>
-              </Row>
-            </Card.Footer>
-          </Card>
-        </Grid>
-      </Grid.Container>
-    </Grid>
-    )
-}
->>>>>>> parent of a17e3a6 (add basic functionality)
-
-  //Add function to fetch data from db.json
-  const fetchData = async () => {
-    const response = await axios.get("http://localhost:8001/bots");
-    setdata(response.data);
-    console.log(data);
-  };
-  //Display the data on the main page
-  useEffect(() => {
-    fetchData();
-  }, []);
-  const Globalstate = useContext(Botcontext);
-  const dispatch = Globalstate.dispatch;
-  console.log(Globalstate);
+function BotCollection() {
   return (
- <div className='bots'>
-{data.map((bots, index) => {
-  return(
-    <Grid className='card' key={index}> 
-    <Grid.Container gap={2} justify="flex-start" >
-      <Grid xs={6} sm={5}>
-        <Card isPressable>
-          <Card.Body css={{ mt: 0 }}>
-            <Card.Image
-              src={bots.avatar_url}
-              objectFit="cover"
-              width="100%"
-              height={250}
-              alt={bots.bot_class}
-            />
-            <Text b>{bots.name}</Text>
-            <Text css={{ color: "$accents7", fontWeight: "$semibold", fontSize: "$sm" }}>{bots.bot_class}</Text>
-            <Text css={{ color: "$accents7", fontWeight: "$semibold", fontSize: "$sm" }}>{bots.catchphrase}</Text>
-            <Grid>
-              <Text color="success" small b>Health</Text>
-              <Progress value={bots.health} color="success" status="success" />
-              <Text color="error" small b>Damage</Text>
-              <Progress value={bots.damage} color="error" status="error" />
-              <Text color="warning" small b>Armor</Text>
-              <Progress value={bots.armor} color="warning" status="warning" />
-            </Grid>
-          </Card.Body>
-          <Card.Footer css={{ justifyItems: "flex-start" }}>
-            <Row wrap="wrap" justify="space-between" align="center">
-            </Row>
-            <Spacer y={2} />
-            <Row wrap="wrap" justify="space-between" align="center">
-                <Button color="gradient" 
-                auto ghost 
-                onClick={() => dispatch({ type: "ADD", payload: bots })}>Join the Force</Button>
-            </Row>          
-          </Card.Footer>
-        </Card>
-      </Grid>
-    </Grid.Container>
-  </Grid>
-  )
-})}
- </div>
+    <div className="bot-container">
+      <Routes>
+        <Route path="/" element={<BotCard />} />
+        <Route path="/army" element={<YourBotArmy />} />
+      </Routes>
+    </div>
   );
-};
+}
 
-export default BotCard;
+export default BotCollection;

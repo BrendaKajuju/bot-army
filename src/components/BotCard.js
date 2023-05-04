a9ab0fe29a7b051f1c4146645764db74fcff11fe
Brenda <brendakajuju777@gmail.com>
2023-05-04 17:14:39 +0300

    add the army component

--- src/components/BotCard.js
+++ src/components/BotCard.js
@@ -1,10 +1,18 @@
-import React, { useState } from 'react';
+//component used to display all bots
+import React, { useState, useEffect, useContext } from 'react';
 import { Card, Grid, Row, Text, Button, Spacer, Progress } from "@nextui-org/react";
 
 const BotCard = ({robot, handleAddBot}) => {
-return (
-          <Grid > 
-<Grid.Container gap={2} justify="flex-start" >
+  const [isJoined, setIsJoined] = useState(false);
+
+  const handleJoin = (event) => {
+    handleAddBot(event, robot);
+    setIsJoined(true);
+  };
+
+  return (
+    <Grid > 
+      <Grid.Container gap={2} justify="flex-start" >
         <Grid xs={6} sm={5}>
           <Card isPressable>
             <Card.Body css={{ mt: 0 }}>
@@ -15,31 +23,35 @@ return (
                 height={250}
                 alt={robot.bot_class}
               />
-            <Text b>{robot.name}</Text>
-            <Text css={{ color: "$accents7", fontWeight: "$semibold", fontSize: "$sm" }}>{robot.bot_class}</Text>
-            <Text css={{ color: "$accents7", fontWeight: "$semibold", fontSize: "$sm" }}>{robot.catchphrase}</Text>
-                <Grid>
+              <Text b>{robot.name}</Text>
+              <Text css={{ color: "$accents7", fontWeight: "$semibold", fontSize: "$sm" }}>{robot.bot_class}</Text>
+              <Text css={{ color: "$accents7", fontWeight: "$semibold", fontSize: "$sm" }}>{robot.catchphrase}</Text>
+              <Grid>
                 <Text color="success" small b>Health</Text>
                 <Progress value={robot.health} color="success" status="success" />
                 <Text color="error" small b>Damage</Text>
                 <Progress value={robot.damage} color="error" status="error" />
                 <Text color="warning" small b>Armor</Text>
                 <Progress value={robot.damage} color="warning" status="warning" />
-               </Grid>
+              </Grid>
             </Card.Body>
             <Card.Footer css={{ justifyItems: "flex-start" }}>
               <Row wrap="wrap" justify="space-between" align="center">
               </Row>
               <Spacer y={2} />
-              <Row wrap="wrap" justify="space-between" align="center">
-              <Button color="gradient" auto ghost onClick={(event) => handleAddBot(event, robot)}>Join the Force</Button>
-              </Row>
+              {isJoined ? (
+                <Text color="success">Joined the force!</Text>
+              ) : (
+                <Row wrap="wrap" justify="space-between" align="center">
+                  <Button color="gradient" auto ghost onClick={handleJoin}>Join the Force</Button>
+                </Row>
+              )}
             </Card.Footer>
           </Card>
         </Grid>
       </Grid.Container>
     </Grid>
-    )
-}
+  );
+};
 
-export default BotCard
+export default BotCard;
